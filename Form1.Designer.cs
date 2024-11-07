ecab1d7ea7f8bf45ea6d238e28c63be0d1a4379b
granfhan <121719521+granfhan@users.noreply.github.com>
2024-11-07 16:23:43 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace BT_7_11_2024
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SimThe = new System.Windows.Forms.DataGridView();
+            this.ThongKe = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.SimThe)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SimThe
+            // 
+            this.SimThe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.SimThe.Location = new System.Drawing.Point(12, 13);
+            this.SimThe.Name = "SimThe";
+            this.SimThe.RowHeadersWidth = 51;
+            this.SimThe.RowTemplate.Height = 24;
+            this.SimThe.Size = new System.Drawing.Size(776, 209);
+            this.SimThe.TabIndex = 0;
+            this.SimThe.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.SimThe_CellContentClick);
+            // 
+            // ThongKe
+            // 
+            this.ThongKe.Location = new System.Drawing.Point(313, 250);
+            this.ThongKe.Name = "ThongKe";
+            this.ThongKe.Size = new System.Drawing.Size(141, 23);
+            this.ThongKe.TabIndex = 1;
+            this.ThongKe.Text = "Thống Kê";
+            this.ThongKe.UseVisualStyleBackColor = true;
+            this.ThongKe.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ThongKe);
+            this.Controls.Add(this.SimThe);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.SimThe)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView SimThe;
+        private System.Windows.Forms.Button ThongKe;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BT_7_11_2024
+{
+  
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        KetNoi conn = new KetNoi();
+        private void SimThe_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM Sim ORDER BY NgayKichHoat";
+            DataSet ds = conn.LayDuLieu(query);
+
+            if (ds != null && ds.Tables.Count > 0)
+            {
+                SimThe.DataSource = ds.Tables[0];
+            }
+            else
+            {
+                MessageBox.Show("Không có dữ liệu để hiển thị.");
+            }
+        }
+    }
+}
+
+    class KetNoi
+    {
+    // Chuỗi kết nối đến SQL Server
+    string conStr = @"Data Source=DESKTOP-075MA7I;Initial Catalog=SimThe;Integrated Security=True;Encrypt=False";
+    SqlConnection conn;
+
+        // Constructor để khởi tạo SqlConnection
+        public KetNoi()
+        {
+            conn = new SqlConnection(conStr);
+        }
+
+        public DataSet LayDuLieu(string truyvan)
+        {
+            try
+            {
+                // Mở kết nối trước khi sử dụng
+                conn.Open();
+
+                // Tạo DataSet để lưu dữ liệu
+                DataSet ds = new DataSet();
+
+                // Tạo SqlDataAdapter với truy vấn và kết nối
+                SqlDataAdapter da = new SqlDataAdapter(truyvan, conn);
+
+                // Đổ dữ liệu vào DataSet
+                da.Fill(ds);
+
+                // Đóng kết nối sau khi lấy dữ liệu
+                conn.Close();
+
+                return ds;
+            }
+            catch (Exception ex)
+            {
+                // Hiển thị thông báo lỗi và đóng kết nối nếu mở
+                MessageBox.Show("Lỗi khi lấy dữ liệu: " + ex.Message);
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                return null;
+            }
+        }
+
+        public bool ThucThi(string truyvan)
+        {
+            try
+            {
+                // Mở kết nối trước khi thực thi lệnh
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(truyvan, conn);
+
+                // Thực thi lệnh SQL
+                int r = cmd.ExecuteNonQuery();
+
+                // Đóng kết nối sau khi thực thi
+                conn.Close();
+
+                return r > 0;
+            }
+            catch (Exception ex)
+            {
+                // Hiển thị thông báo lỗi và đóng kết nối nếu mở
+                MessageBox.Show("Lỗi khi thực thi lệnh: " + ex.Message);
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                return false;
+            }
+        }
+    
+}
